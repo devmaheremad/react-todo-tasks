be444c38fc6616882f492080a0a2acf30282ffae
devmaheremad <mashmaher96@gmail.com>
2020-12-22 19:20:34 +0200

    some fixes

--- src/Components/ToDoListAdd/ToDoListAdd.js
+++ src/Components/ToDoListAdd/ToDoListAdd.js
@@ -28,10 +28,10 @@ class ToDoListAdd extends Component {
             <div className='to-do-list-add'>
                 <form method="post" onChange={this.handelChange} onSubmit={this.handelSubmit} >
                 <label className="task" htmlFor="task">
-                    <input type="text" id="task" placeholder='Type your task name here!' defaultValue={this.state.task} required/>
+                    <input type="text" id="task" placeholder='Type your task name here!' value={this.state.task} required/>
                 </label>
                 <label className="priority" htmlFor="priority">
-                    <input type="text" id="priority" placeholder='Type your priority here!' defaultValue={this.state.priority} required/>
+                    <input type="text" id="priority" placeholder='Type your priority here!' value={this.state.priority} required/>
                 </label>
                 <input type="submit" value='Add new task'/>
                 </form>
